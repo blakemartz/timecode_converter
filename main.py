51791d4fc85b3ea216c7c1108b4b003d51acab27
Blake Martz <blakemartz@gmail.com>
2023-04-08 15:53:43 -0700

    Revert convert_timecode function, add pandas output
    Original convert_timecode function yielded closer results to the output from adobe premiere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import math
+import pandas as pd
 
 
 def timecode_to_frames(timecode, fps):
@@ -18,23 +19,18 @@ def frames_to_timecode(frames, fps):
     frames %= fps
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}:{frames:02d}"
 
-
 def convert_timecode(timecode):
-    frames_2398 = timecode_to_frames(timecode, 23.976)  # Use the exact frame rate
-    time_in_seconds = frames_2398 * (1001 / 24000)  # Convert to real-time (in seconds)
+    frames_2398 = timecode_to_frames(timecode, 23.976)
+    frames_2997 = frames_2398 * (30000 / 1001) / (24000 / 1001)
 
-    # Convert real-time to frames at 29.97 fps drop-frame
-    frames_2997 = time_in_seconds * (30000 / 1001)
-    drop_frames = 2 * math.floor(frames_2997 / 1800) - math.floor(math.floor(frames_2997 / 1800) / 10)
-
-    frames_2997_adjusted = round(frames_2997 - drop_frames)
+    drop_frames = math.floor(frames_2997 / 17982) * 18 + math.floor((frames_2997 % 17982) / 1798)
+    frames_2997_adjusted = frames_2997 + drop_frames
     timecode_2997 = frames_to_timecode(frames_2997_adjusted, 30)
 
     return timecode_2997.replace(':', ';', 2)
 
-
 input_csv = 'input_timecodes.csv'
-output_csv = 'output_timecodes_accu.csv'
+output_csv = 'output_timecodes.csv'
 
 with open(input_csv, 'r') as infile, open(output_csv, 'w') as outfile:
     csv_reader = csv.reader(infile)
@@ -44,3 +40,12 @@ with open(input_csv, 'r') as infile, open(output_csv, 'w') as outfile:
         timecode_2398 = row[0]
         timecode_2997 = convert_timecode(timecode_2398)
         csv_writer.writerow([timecode_2398, timecode_2997])
+
+
+output_csv = 'output_timecodes.csv'
+
+# Read the output CSV file using pandas
+df = pd.read_csv(output_csv, header=None, names=['Timecode 23.976 fps', 'Timecode 29.97 fps Drop-frame'])
+
+# Display the DataFrame
+print(df)
